4e30d3aac4caaf7fedae69b8889c8195c2dfb755
kushal nepal <nepalkushal74@gmail.com>
2024-02-12 17:21:45 -0800

    .

---
+++ api.route.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const authrouter = require("./controllers/auth.controller");
+const userrouter =require("./controllers/user.controller");
+
+
+router.use("/auth",authrouter);
+router.use("/user",userrouter);
+
+
+module.exports = router;
