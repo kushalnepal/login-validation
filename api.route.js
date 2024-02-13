cf6dec0b4d05cf35479cc6a3b2de8ac389288c91
kushal nepal <nepalkushal74@gmail.com>
2024-02-12 17:09:46 -0800

    shorthand routefiles

--- api.route.js
+++ api.route.js
@@ -1,9 +1,6 @@
 const router = require('express').Router()
 const authrouter = require("./controllers/auth.controller");
 const userrouter =require("./controllers/user.controller");
-
-
 router.use("/auth",authrouter);
 router.use("/user",userrouter);
-
-module.exports = router
+module.exports = router   
