93b3a623c5d987bb21ec248f40169253337d8b78
kushalnepal <31437959+kushalnepal@users.noreply.github.com>
2024-02-12 17:21:03 -0800

    Delete api.route.js

--- api.route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const authrouter = require("./controllers/auth.controller");
-const userrouter =require("./controllers/user.controller");
-
-
-router.use("/auth",authrouter);
-router.use("/user",userrouter);
-
-module.exports = router
